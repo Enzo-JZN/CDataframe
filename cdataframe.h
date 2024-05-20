c3ad992a46e2c34079777f29a0f8a83874442501
Enzo-JZN <130388912+Enzo-JZN@users.noreply.github.com>
2024-05-20 18:34:47 +0200

    Update cdataframe.h

--- cdataframe.h
+++ cdataframe.h
@@ -1,11 +1,9 @@
-
-
 #ifndef cdataframe_h
 #define cdataframe_h
 
-#include "colonne.h"
+#include "colonnes.h"
 
-// On souhait d'abord créer un objet et une structure colonne :
+// On souhaite d'abord créer un objet et une structure colonne :
 // Création de l'objet et des attributs d'un CDATAFRAM
 typedef struct {
     char titre[20];
@@ -24,14 +22,16 @@ void afficher_CDATAFRAME_ligne (CDataframe* Datafram, int nb_ligne);
 void afficher_CDATAFRAME_colonne (CDataframe* Datafram, int nb_colonne);
 
 // fonctionnalités 3 :
-
 void ajouter_ligne_valeurs_CDataFrame (CDataframe *DataFrame);
 void supprimer_ligne_valeurs_CDataFrame (CDataframe *DataFrame);
-void ajouter_colonne_valeurs_CDataFrame (CDataframe *DataFrame);
-void supprimer_colonne_valeurs_CDataFrame (CDataframe *DataFrame);
-void renommer_collone_CDataFrame (CDataframe *DataFrame, char* ancien_titre, char* noveau_titre);
-int occurence_valeur_Cdataframe (CDataframe* DataFrame, int x);
-void remplacer_valeur_CDataFrame (CDataframe* DataFrame, int nouvelle_valeur, int num_ligne, int num_colonne);
+
+void ajouter_colonne_valeurs_CDataFrame (CDataframe *DataFrame, COLUMN *column);
+void supprimer_colonne_valeurs_CDataFrame (CDataframe *DataFrame, int pos);
+
+void renommer_colonne_CDataFrame (CDataframe *DataFrame, char *titre, char *nouveau_titre);
+// int occurence_valeur_Cdataframe (CDataframe* DataFrame, COL_TYPE x);
+int occurence_valeur_Cdataframe(CDataframe* DataFrame, ENUM_TYPE type, COL_TYPE value);
+int remplacer_valeur_CDataFrame (CDataframe* DataFrame, int num_ligne, int num_colonne, COL_TYPE nouvelle_valeur);
 
 void afficher_nom_colonne_cdataframe (CDataframe* DataFrame);
 
@@ -41,5 +41,6 @@ void Afficher_nb_colonnes_Cdataframe (CDataframe* Dataframe);
 void valeurs_egale_x_CdataFrame (CDataframe* DataFrame, int x);
 void valeurs_sup_x_CdataFrame (CDataframe* DataFrame, int x);
 void valeurs_inf_x_CdataFrame (CDataframe* DataFrame, int x);
+int verifier_existence_valeur(CDataframe* DataFrame, ENUM_TYPE type, COL_TYPE value);
 
 #endif /* cdataframe_h */
